d535b7503a75a5a7b97f1c106ce4a759d7cbf348
Александра Другова <aleksandradrugova23@gmail.com>
2026-04-03 00:17:54 +0300

    Добавлен метод и вменен, возвращающий более полные данные - и год, и расчеты за год. Дополнена документация, внесены незачительные корректировки по синтаксису.

--- tasks/DataTypes_Task1/Program.cs
+++ tasks/DataTypes_Task1/Program.cs
@@ -1,59 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataTypes_Task1
+namespace DataTypesTask1
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            var initial_deposit = decimal.Parse(GetStringFromConsole("Введите начальный вклад (положительное число): "));
-            var years = int.Parse(GetStringFromConsole("Введите количество лет (положительное целое число): "));
-            var interest_rate = decimal.Parse(GetStringFromConsole("Введите годовую процентную ставку (положительное число): "));
+            try
+            {
+                var initialDeposit = decimal.Parse(GetStringFromConsole("Введите начальный вклад (положительное число): "));
+                var years = uint.Parse(GetStringFromConsole("Введите количество лет (положительное целое число): "));
+                var interestRate = decimal.Parse(GetStringFromConsole("Введите годовую процентную ставку (положительное число): "));
 
-            Console.WriteLine(GetStringOfCalculations(initial_deposit, years, interest_rate));
+                var calculations = new Dictionary<uint, decimal>(GetCalculationsDictionary(initialDeposit, years, interestRate));
+
+                OutputCalculations(calculations);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Ошибка ввода числа.\n{ex.Message}");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine($"Ошибка ввода неотрицательного числа.\n{ex.Message}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
-
+        
         /// <summary>
-        /// Выводит список накоплений на экран.
+        /// Выводит расчеты в надлежащем виде.
         /// </summary>
-        static string GetStringOfCalculations(decimal initial_deposit, int years, decimal interest_rate)
+        /// <param name="calculations">Словарь "год" - "сумма накоплений".</param>
+        public static void OutputCalculations(Dictionary<uint, decimal> calculations)
         {
+            // Изменение языка и региона для вывода вещественного с точкой, а не с запятой.
             System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-us");
-            var sums = new List<decimal>(GetAccumulatedSumsList(initial_deposit, years, interest_rate));
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < years; i++)
-            {
-                result.Append($"Год {i + 1}: {Decimal.Round(sums[i], 2)} руб.\n");
-            }
 
-            return result.ToString();
+            foreach (var pair in calculations)
+            {
+                Console.WriteLine($"Год {pair.Key}: {Decimal.Round(pair.Value, 2)} руб.");
+            }
         }
 
         /// <summary>
-        /// Формирует список накоплений.
+        /// Расчитывает сумму накоплений за каждый год.
         /// </summary>
-        static IEnumerable<decimal> GetAccumulatedSumsList(decimal initial_deposit, int years, decimal interest_rate)
+        /// <param name="initialDeposit">Начальный вклад.</param>
+        /// <param name="years">Количество лет.</param>
+        /// <param name="interestRate">Годовая процентная ставка.</param>
+        /// <returns>Словарь, где ключ - год, значение - сумма накоплений.</returns>
+        public static Dictionary<uint, decimal> GetCalculationsDictionary(decimal initialDeposit, uint years, decimal interestRate)
         {
+            decimal lowerLimit = 1;
+
+            CheckNumber(initialDeposit, lowerLimit);
+            CheckNumber(years, lowerLimit);
+            CheckNumber(interestRate, lowerLimit);
+
+            Dictionary<uint, decimal> result = new Dictionary<uint, decimal>();
             const decimal MaxPercent = 100;
-            decimal proportion = interest_rate / MaxPercent;
-            for (int i = 0; i < years; i++)
+
+            decimal proportion = interestRate / MaxPercent;
+
+            for (uint i = 0; i < years; i++)
             {
-                initial_deposit += initial_deposit * proportion;
-                yield return initial_deposit;
+                initialDeposit += initialDeposit * proportion;
+                result.Add((i+1), initialDeposit);
             }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Проверяет соблюдение границ числа.
+        /// </summary>
+        /// <param name="number">Проверяемое число.</param>
+        /// <param name="lowerLimit">Нижняя граница.</param>
+        /// <exception cref="ArgumentException">Выбрасывается при выходе за установленные границы.</exception>
+        public static void CheckNumber(decimal number, decimal lowerLimit)
+        {
+            if (number < lowerLimit)
+                throw new ArgumentException($"\nОжидался ввод неотрицательного числа. Получено: {number}.");
         }
 
         /// <summary>
         /// Реализует интерактив.
         /// </summary>
-        /// <remarks>
-        /// Выводит сообщение и считывает строку для преобразования потом в нужный тип данных.
-        /// </remarks>
-        static string GetStringFromConsole(string message)
+        /// <param name="message">Сообщение для пользователя.</param>
+        /// <returns>Строка ввода для дальнейшего преобразования в нужный тип.</returns>
+        public static string GetStringFromConsole(string message)
         {
             Console.Write(message);
             return Console.ReadLine();
