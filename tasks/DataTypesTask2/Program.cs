130752fd9b9ce3a3fb00bb9f6b94de87c7cc9299
Александра Другова <aleksandradrugova23@gmail.com>
2026-04-03 08:58:58 +0300

    Названия проектов изменены в пользу соответствия стилю camelCase.

---
+++ tasks/DataTypesTask2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypesTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(GetDiamondString(21));
+        }
+
+        static uint[,] GetDiamondStringsIndexes(int n)
+        {
+            var half = (int)n / 2 + 1;
+
+            
+        }
+
+    }
+}
+
+//StringBuilder upperHalf = new StringBuilder();
+//StringBuilder lowerHalf = new StringBuilder();
+
+//for (int i = 1; i < half; i++)
+//{
+//    StringBuilder halfLine = new StringBuilder();
+//    for (int j = 1; j < half; j++)
+//    {
+//        halfLine.Append((i + j == half)
+//            ? 'X'
+//            : ' '
+//        );
+//    }
+//    var reversedHalf = new string(halfLine.ToString().Reverse().ToArray());
+//    StringBuilder singleLine = new StringBuilder($"{halfLine}{reversedHalf.ToString().Substring(1)}\n");
+//    upperHalf.Append(singleLine);
+//    if (i != half - 1)
+//        lowerHalf.Insert(0, singleLine);
+//}
+
+//lowerHalf.Remove(lowerHalf.Length - 1, 1);
+
+//return upperHalf.ToString() + lowerHalf.ToString();
